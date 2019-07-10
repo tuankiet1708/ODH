1da4988246facf8f4cdf930f6bcb9ab421e06266
hieuvijjo <31264468+hieuvijjo@users.noreply.github.com>
2019-07-10 09:07:52 +0700

    Update enen_Oxford.js

--- src/dict/enen_Oxford.js
+++ src/dict/enen_Oxford.js
@@ -9,9 +9,6 @@ class encn_Oxford {
     }
 
     async displayName() {
-        let locale = await api.locale();
-        if (locale.indexOf('CN') != -1) return '牛津英汉双解';
-        if (locale.indexOf('TW') != -1) return '牛津英汉双解';
         return 'Oxford EN->EN Dictionary';
     }
 
@@ -223,11 +220,9 @@ class encn_Oxford {
                 span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
                 span.tran {margin:0; padding:0;}
                 span.eng_tran {margin-right:3px; padding:0;}
-                span.chn_tran {color:#0d47a1;}
                 ul.sents {font-size:0.9em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
                 li.sent  {margin:0; padding:0;}
                 span.eng_sent {margin-right:5px;}
-                span.chn_sent {color:#0d47a1;}
             </style>`;
         return css;
     }
